708ea481d0dc57e5363b22cca822978abf58081b
Nishad <nishad@Nishads-MacBook-Air.local>
2021-11-10 06:57:19 -0800

    add JPA query annotation

--- src/main/java/com/learner/ipldashboard/controller/TeamController.java
+++ src/main/java/com/learner/ipldashboard/controller/TeamController.java
@@ -1,5 +1,9 @@
 package com.learner.ipldashboard.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import com.learner.ipldashboard.model.Match;
 import com.learner.ipldashboard.model.Team;
 import com.learner.ipldashboard.repository.MatchRepository;
 import com.learner.ipldashboard.repository.TeamRepository;
@@ -7,6 +11,7 @@ import com.learner.ipldashboard.repository.TeamRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,5 +33,18 @@ public class TeamController {
         team.setLatestMatches(this.matchRepository.findLatestMatchesByTeam(teamName, 4));
 
         return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+
+        return this.matchRepository.getMatchesByTeamBetweenDates(
+            teamName, 
+            startDate, 
+            endDate
+        );
     }
 }
